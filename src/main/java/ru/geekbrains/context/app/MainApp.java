789e871dbb6706a7ee64a648044bbbfa3474cf1c
Big_comp <anerdy@yandex.ru>
2020-09-10 18:09:45 +0300

    init

---
+++ src/main/java/ru/geekbrains/context/app/MainApp.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.context.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationContext.class);
+
+        context.close();
+
+    }
+}
